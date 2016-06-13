428a35e2e357c144bf3de861a81caef9abbd1c59
Tomasz Nowiński <tomasz.n@showroom.com>
2016-06-13 16:13:32 +0200

    Request/GetLabel: use CollectShipmentsByEDTNs trait

--- src/Request/GetLabel.php
+++ src/Request/GetLabel.php
@@ -10,54 +10,7 @@ use Sheepla\Request\Shipment\ShipmentByEDTN;
  */
 class GetLabel extends AbstractRequest
 {
-    /**
-     * @var ShipmentByEDTN[]
-     * @JMS\AccessType("public_method")
-     * @JMS\Accessor(setter = "addShipment")
-     * @JMS\Type("array<Sheepla\Request\Shipment\ShipmentByEDTN>")
-     * @JMS\XmlList(entry="shipmentEDTN")
-     */
-    private $shipments = [];
-
-    /**
-     * Add shipment
-     *
-     * @param ShipmentByEDTN $shipmentByEDTN
-     * @return $this
-     */
-    public function addShipment(ShipmentByEDTN $shipmentByEDTN)
-    {
-        $this->shipments[] = $shipmentByEDTN;
-
-        return $this;
-    }
-
-    /**
-     * Remove shipment
-     *
-     * @param ShipmentByEDTN $shipmentByEDTN
-     * @return $this
-     */
-    public function removeShipment(ShipmentByEDTN $shipmentByEDTN)
-    {
-        $key = array_search($shipmentByEDTN, $this->shipments, true);
-
-        if ($key !== false) {
-            unset($this->shipments[$key]);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get shipments
-     *
-     * @return Shipment\ShipmentByEDTN[]
-     */
-    public function getShipments()
-    {
-        return $this->shipments;
-    }
+    use CollectShipmentsByEDTNs;
 
     /**
      * Get request method
