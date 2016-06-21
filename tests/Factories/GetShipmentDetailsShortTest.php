d7aa70cb2e0a258849d95f025bf78d8769a06ca3
Tomasz Nowiński <tomasz.n@showroom.com>
2016-06-21 11:11:47 +0200

    Test GetShipmentDetailsShort factory

---
+++ tests/Factories/GetShipmentDetailsShortTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use Sheepla\Factories\GetShipmentDetailsShort as Factory;
+use \Sheepla\Request\Shipment\ShipmentByEDTN;
+
+class GetShipmentDetailsShortTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $apiKey = 'API_KEY';
+
+        $factory = new Factory($apiKey);
+
+        $this->assertInstanceOf(Factory::class, $factory);
+        $this->assertEquals($apiKey, $factory->getApiKey());
+
+        return $factory;
+    }
+
+    /**
+     * @param Factory $factory
+     * @return ShipmentByEDTN
+     * @depends testConstructor
+     */
+    public function testCreateShipmentByEDTNRequest(Factory $factory)
+    {
+        $edtn = '123xyz';
+
+        $shipment = $factory->createShipmentByEDTNRequest($edtn);
+
+        $this->assertInstanceOf(ShipmentByEDTN::class, $shipment);
+        $this->assertEquals($edtn, $shipment->getEdtn());
+    }
+
+    /**
+     * @param Factory $factory
+     * @depends testConstructor
+     */
+    public function testGetShipmentDetailsShortRequest(Factory $factory)
+    {
+        $shipments = $factory->createRequest();
+
+        $this->assertInstanceOf(\Sheepla\Request\GetShipmentDetailsShort::class, $shipments);
+    }
+
+    /**
+     * @param Factory $factory
+     * @depends testConstructor
+     */
+    public function testGCreateResponse(Factory $factory)
+    {
+        $shipments = $factory->createResponse();
+
+        $this->assertInstanceOf(\Sheepla\Response\GetShipmentDetailsShort::class, $shipments);
+    }
+
+
+}
