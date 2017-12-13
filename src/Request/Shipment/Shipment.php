13e8aba9df475e8e52a02dbc444ab0b2e76309c5
Arkadiusz Laszewski <arek.l@shwrm.com>
2017-12-13 15:46:19 +0100

    JMS Type missing in Shipment SHWRM-1511

--- src/Request/Shipment/Shipment.php
+++ src/Request/Shipment/Shipment.php
@@ -28,6 +28,7 @@ class Shipment
     /**
      * @JMS\AccessType("public_method")
      * @JMS\XmlElement(cdata = false)
+     * @JMS\Type("string")
      */
     private $carrierAccount;
 
@@ -40,12 +41,14 @@ class Shipment
     /**
      * @JMS\AccessType("public_method")
      * @JMS\XmlElement(cdata = false)
+     * @JMS\Type("string")
      */
     private $description;
 
     /**
      * @JMS\AccessType("public_method")
      * @JMS\XmlElement(cdata = false)
+     * @JMS\Type("boolean")
      */
     private $confirmAfterCreate;
 
